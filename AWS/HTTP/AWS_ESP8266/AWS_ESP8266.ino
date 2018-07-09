6710b1a68bedee112681af6ba27e4746823efdd4
nhamtung <tung.nham@ntq-solution.com.vn>
2018-07-09 15:54:03 +0700

    AWS

---
+++ AWS/HTTP/AWS_ESP8266/AWS_ESP8266.ino
@@ -0,0 +1,50 @@
+#include <ESP8266WiFi.h>
+#include <AmazonIOTClient.h>
+#include "ESP8266AWSImplementations.h"
+
+Esp8266HttpClient httpClient;
+Esp8266DateTimeProvider dateTimeProvider;
+
+AmazonIOTClient iotClient;
+ActionError actionError;
+
+char *ssid="CH3CHO + H2";
+char *password="ch3ch2oh";
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // Connect to WAP
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  iotClient.setAWSRegion("eu-west-1");
+  iotClient.setAWSEndpoint("amazonaws.com");
+  iotClient.setAWSDomain("avt6g3kvwjn2o.iot.ap-southeast-1.amazonaws.com");
+  iotClient.setAWSPath("/things/NodeMcuEsp8266/shadow");
+  iotClient.setAWSKeyID("AKIAJ3RDBZS7WIJ5PHWQ");
+  iotClient.setAWSSecretKey("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");
+  iotClient.setHttpClient(&httpClient);
+  iotClient.setDateTimeProvider(&dateTimeProvider);
+}
+
+void loop(){
+  char* shadow = "{\"state\":{\"reported\": {\"foobar\": \"bar\"}}}";
+
+  char* result = iotClient.update_shadow(shadow, actionError);
+  Serial.print(result);
+
+  delay(60000);
+}
+
